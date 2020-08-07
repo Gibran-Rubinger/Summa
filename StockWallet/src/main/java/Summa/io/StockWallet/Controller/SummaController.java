dd8b2022dfee121805b74cf7402da585344cc6c5
Gibran Rubinger <gibranrubinger@gmail.com>
2020-08-07 09:33:14 +0100

    up
    up

--- StockWallet/src/main/java/Summa/io/StockWallet/Controller/SummaController.java
+++ StockWallet/src/main/java/Summa/io/StockWallet/Controller/SummaController.java
@@ -1,5 +1,8 @@
 package Summa.io.StockWallet.Controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,37 +11,102 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Summa.io.StockWallet.fetchData.Stock;
+
 @RestController
 public class SummaController {
+
+	 
+
+	Set<Stock> FTSE100;
+
+	public SummaController() {
+		FTSE100 = new HashSet<Stock>();
+	}
 
 	@RequestMapping("/index")
 	public String test() {
 		return "index";
 	}
 
-	final String url = "https://www.investing.com/indices/iseq-overall-components";
+	final String url = "https://www.londonstockexchange.com/indices/ftse-100/constituents/table";
 
 	@GetMapping("/fetch")
-	public String FetchIseq() {
-		String stockName = "";
+	public Stock FetchIseq(Stock stock) {
+
 		try {
 			final Document result = Jsoup.connect(url).get();
 
 //			System.out.println(result.outerHtml());
 
-			for (Element row : result.select("table.genTbl.closedTbl.crossRatesTbl.elpTbl.elp25 tr")) {
-				if (row.select("td.bold.left.noWrap.elp.plusIconTd").text().equals("")) {
+			for (Element row : result.select("table.full-width.ftse-index-table-table")) {
+				 String stockCd = "";
+				 String stockNm = "";
+				 String stockMk = "";
+
+				 String textPrice = "";
+				 String transitionPrice = "";
+				 Double stockPr = 0.00;
+
+				 String textChange = "";
+				 String transitionChange = "";
+				 Double stockCh = 0.00;
+
+				 String textChangeN = "";
+				 String transitionChangeN = "";
+				 Double stockChN = 0.00;
+
+				 Double finalStockChange = 0.00;
+
+				 String textChPer = "";
+				 String transitionChPer = "";
+				 Double stockCp = 0.00;
+				if (row.select("td.clickable.bold-font-weight.instrument-tidm.gtm-trackable.td-with-link").text()
+						.equals("")) {
 					continue;
 				} else {
-					final String stock = row.select("td.bold.left.noWrap.elp.plusIconTd").text();
-					System.out.println(stock);
-					stockName = stock;
+					stockCd = row.select("td.clickable.bold-font-weight.instrument-tidm.gtm-trackable.td-with-link")
+							.text();
+//					System.out.println(stockCd);
+					stockNm = row.select("td.clickable.instrument-name.gtm-trackable.td-with-link").text();
+//					System.out.println(stockNm);
+					stockMk = row.select("td.instrument-marketcapitalization.hide-on-landscape").text();
+					System.out.println("este é um numero pro Michael"+stockMk);
+					transitionPrice = row.select("td.instrument-lastprice").text();
+					textPrice = transitionPrice.replace(",", "");
+					stockPr = Double.parseDouble(textPrice);
+					System.out.println("este é um numero pro Michael"+stockPr);
+					
+					transitionChange = row.select("td.instrument-netchange.hide-on-landscape.positivechange").text();
+					textChange = transitionChange.replace(",", "");
+					stockCh = Double.parseDouble(textChange);
+					System.out.println("este é um numero pro Michael"+stockCh);
+					
+					transitionChangeN = row.select("td.instrument-netchange.hide-on-landscape.negativechange").text();
+					textChangeN = transitionChangeN.replace(",", "");
+					stockChN = Double.parseDouble(textChangeN);
+					System.out.println("este é um numero pro Michael"+stockChN);
+					
+					transitionChPer = row.select("td.instrument-percentualchange.hide-on-landscape.gtm-trackable")
+							.text();
+					textChPer = transitionChPer.replace(",", "");
+					stockCp = Double.parseDouble(textChPer);
+					System.out.println(stockCp);
 				}
+				if (stockCh != 0.00) {
+					finalStockChange = stockCh;
+				} else {
+					finalStockChange = stockChN;
+				}
+				FTSE100.add(new Stock(stockCd, stockNm, stockMk, stockPr, finalStockChange, stockCp));
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		return stockName;
+		for (Stock st : FTSE100) {
+			System.out.println(st);
+		}
+		return stock;
 	}
 
 }
--- StockWallet/src/main/java/Summa/io/StockWallet/fetchData/Stock.java
+++ StockWallet/src/main/java/Summa/io/StockWallet/fetchData/Stock.java
@@ -3,19 +3,30 @@ package Summa.io.StockWallet.fetchData;
 import java.time.LocalDateTime;
 
 public class Stock {
-	
+
+	private String code = "";
 	private String name = "";
-	private Double lastPrice = 0.00;
-	private Double highPrice = 0.00;
-	private Double lowPrice = 0.00;
+	private String marketCap = "";
+	private Double price = 0.00;
+	private Double priceChange = 0.00;
+	private Double priceChangePer = 0.00;
 	LocalDateTime dTime = LocalDateTime.now();
 
-	public Stock(String name, Double lastPrice, Double highPrice, Double lowPrice, LocalDateTime dTime) {
+	public Stock(String code, String name, String marketCap, Double price, Double priceChange, Double priceChangePer) {
+		this.code = code;
 		this.name = name;
-		this.lastPrice = lastPrice;
-		this.highPrice = highPrice;
-		this.lowPrice = lowPrice;
-		this.dTime = dTime;
+		this.marketCap = marketCap;
+		this.price = price;
+		this.priceChange = priceChange;
+		this.priceChangePer = priceChangePer;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
 	}
 
 	public String getName() {
@@ -26,30 +37,52 @@ public class Stock {
 		this.name = name;
 	}
 
-	public Double getLastPrice() {
-		return lastPrice;
+	public String getMarketCap() {
+		return marketCap;
 	}
 
-	public void setLastPrice(Double lastPrice) {
-		this.lastPrice = lastPrice;
+	public void setMarketCap(String marketCap) {
+		this.marketCap = marketCap;
 	}
 
-	public Double getHighPrice() {
-		return highPrice;
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setHighPrice(Double highPrice) {
-		this.highPrice = highPrice;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
-	public Double getLowPrice() {
-		return lowPrice;
+	public Double getPriceChange() {
+		return priceChange;
 	}
 
-	public void setLowPrice(Double lowPrice) {
-		this.lowPrice = lowPrice;
+	public void setPriceChange(Double priceChange) {
+		this.priceChange = priceChange;
 	}
-	
-	
-	
+
+	public Double getPriceChangePer() {
+		return priceChangePer;
+	}
+
+	public void setPriceChangePer(Double priceChangePer) {
+		this.priceChangePer = priceChangePer;
+	}
+
+	public LocalDateTime getdTime() {
+		return dTime;
+	}
+
+	public void setdTime(LocalDateTime dTime) {
+		this.dTime = dTime;
+	}
+
+//	creating a toString to Override it.
+	@Override
+	public String toString() {
+		return "      CODE: " + code + "       STOCK NAME: " + name + "        MARKET CAP:  " + marketCap
+				+ " (m).        PRICE:  £" + price + "         CHANGE: " + priceChange + "       CHANGE %:"
+				+ priceChangePer + "         DATE: " + dTime
+				+ "\n\n\n___________________________________________________________________________________________________________________________________________\n";
+	}
 }
